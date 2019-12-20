abc77a371329eb85f054bf25a8dfca95e8d5c824
Devin Shoemaker <devinshoe@gmail.com>
2019-12-20 06:55:56 -0600

    home: import IonicModule

--- apps/todo/src/app/home/home.module.ts
+++ apps/todo/src/app/home/home.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 import { HomeRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    IonicModule
   ]
 })
 export class HomeModule { }
